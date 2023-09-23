81770c09035283c22aaf5ee3cfb66c00d341ccf0
Leonardo Delfino <leonardodelfino@gmail.com>
2023-09-23 01:37:23 -0300

    fix: docker problems

---
+++ currency-front/src/config/env.ts
@@ -0,0 +1,7 @@
+const config = {
+    currencyApiUser: process.env.REACT_APP_CURRENCY_API_USER || '',
+    currencyApiPassword:  process.env.REACT_APP_CURRENCY_API_PASSWORD || '',
+    currencyApiBaseUrl:  process.env.REACT_APP_CURRENCY_API_BASE_URL || ''
+}
+
+export default config;
